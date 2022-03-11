8b2af8e4fdc4c2b24b7bbc249e11f4297cfbd028
DESKTOP-46TCRJJ\Andrew <46>
2022-03-11 12:14:47 +0200

    add kafka for local run and in docker add commands and .bat

---
+++ kafka/local/+start.bat
@@ -0,0 +1,3 @@
+start zookeeper-server-start.bat C:\Users\Andrew\Downloads\kafka\config\zookeeper.properties
+timeout 10
+start kafka-server-start.bat C:\Users\Andrew\Downloads\kafka\config\server.properties
